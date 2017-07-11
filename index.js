98fe4235f27566ab0249a83ca14da02b9ca57f0b
gyorou <gyorou@tjjtds.me>
2017-07-11 15:17:47 +0900

    initial

---
+++ index.js
@@ -0,0 +1,20 @@
+const hexo = hexo || {};
+const config = hexo.config;
+const fs = require("hexo-fs");
+const path = require("path");
+
+hexo.extend.generator.register("statics", function(){
+
+  let statics_dir = config.statics_dir;
+  let files = fs.listDirSync("statics");
+  return files.map(function(file){
+    return  {
+      path: file,
+      data: function(){
+        return fs.createReadStream(path.join(statics_dir, file));
+      }
+    };
+  });
+});
+
+
